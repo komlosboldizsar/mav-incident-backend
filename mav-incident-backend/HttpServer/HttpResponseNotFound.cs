1e3b3ad9ffc35f51955b72667c6c488c0a94f560
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-03 18:57:22 +0100

    Add 404 response class

---
+++ mav-incident-backend/HttpServer/HttpResponseNotFound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mav_incident_backend.HttpServer
+{
+    public class HttpResponseNotFound : HttpResponse
+    {
+        public HttpResponseNotFound(HttpRequest request)
+        {
+            this.ResponseCode = HttpResponseCode.S_404_NotFound;
+            this.Headers["Server"] = "IGNO1V";
+            this.Body = "<h1>404 Internal Server Error</h1>Nooo, noo.";
+        }
+    }
+}
