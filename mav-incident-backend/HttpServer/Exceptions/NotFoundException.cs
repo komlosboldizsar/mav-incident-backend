6abc6a6218fc5345bdf21b376fe38e981f5f95dc
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-10 14:16:37 +0100

    Add NotFoundException to HTTP server

---
+++ mav-incident-backend/HttpServer/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mav_incident_rest.HttpServer.Exceptions
+{
+    class NotFoundException : Exception
+    {
+    }
+}
