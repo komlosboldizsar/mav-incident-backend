6747b276d5b0cf6793ccc602145d448f98a22d3d
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-10 15:16:10 +0100

    Add 2 empty projects for processing XML feed

---
+++ mav-incident-processor-service/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mav_incident_processor_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
