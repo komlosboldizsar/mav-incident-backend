592b51c0e34125e939336ed2b447131db60126d1
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-16 18:39:36 +0100

    Add argument processor and arguments to incident processor service

--- mav-incident-processor-service/Program.cs
+++ mav-incident-processor-service/Program.cs
@@ -1,9 +1,11 @@
-﻿using mav_incident_dba;
+﻿using CommandLine;
+using mav_incident_dba;
 using mav_incident_processor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace mav_incident_processor_service
@@ -12,13 +14,64 @@ namespace mav_incident_processor_service
     {
         static void Main(string[] args)
         {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o => Do(o))
+                   .WithNotParsed<Options>((errs) => HandleParseError(errs));
+        }
+        private static void Do(Options options)
+        {
             IncidentDatabase.Instance.Init();
-            RssFeedProcessor feedProcessor = new RssFeedProcessor("https://www.mavcsoport.hu/mavinform/rss.xml");
-            feedProcessor.Do();
-            OldEntryUpdater oldEntryUpdater = new OldEntryUpdater(14, false);
-            oldEntryUpdater.Do();
-            Console.ReadKey();
+            OldEntryUpdater oldEntryUpdater = new OldEntryUpdater(options.UpdateOldMaxAge, false);
+            RssFeedProcessor feedProcessor = new RssFeedProcessor(options.FeedURL);
+            int oldEntryUpdaterTimer = 0;
+            int feedProcessorTimer = 0;
+            do
+            {
+                if ((oldEntryUpdaterTimer == 0) && options.UpdateOld)
+                    oldEntryUpdater.Do();
+                if ((feedProcessorTimer == 0) && options.ProcessNews)
+                    feedProcessor.Do();
+                oldEntryUpdaterTimer++;
+                feedProcessorTimer++;
+                if (oldEntryUpdaterTimer == options.UpdateOldFrequency)
+                    oldEntryUpdaterTimer = 0;
+                if (feedProcessorTimer == options.ProcessNewsFrequency)
+                    feedProcessorTimer = 0;
+                if (!options.RunOnce)
+                    Thread.Sleep(60000);
+            } while (!options.RunOnce);
             IncidentDatabase.Instance.DeInit();
         }
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+            foreach (Error e in errs)
+                Console.WriteLine(e.ToString());
+        }
+        class Options
+        {
+
+            [Option('o', "run-once", Default = false, HelpText = "Run only once or in a loop.", Required = false)]
+            public bool RunOnce { get; set; }
+
+            [Option('f', "feed-url", Default = "https://www.mavcsoport.hu/mavinform/rss.xml", HelpText = "URL of MÁVINFORM RSS feed.", Required = false)]
+            public string FeedURL { get; set; }
+
+            [Option('u', "update-old", Default = true, HelpText = "Update older, known entries.", Required = false)]
+            public bool UpdateOld { get; set; }
+
+            [Option("update-old-age", Default = 14, HelpText = "Maximal age of entries that are updated.", Required = false)]
+            public int UpdateOldMaxAge { get; set; }
+
+            [Option("update-old-frequency", Default = 10, HelpText = "Frequency of updating old incident entries in minutes.", Required = false)]
+            public int UpdateOldFrequency { get; set; }
+
+            [Option('n', "process-news", Default = true, HelpText = "Process new feed items and add them to the database.", Required = false)]
+            public bool ProcessNews { get; set; }
+
+            [Option("process-news-frequency", Default = 5, HelpText = "Frequency of processing news feed in minutes.", Required = false)]
+            public int ProcessNewsFrequency { get; set; }
+
+        }
+
     }
 }
