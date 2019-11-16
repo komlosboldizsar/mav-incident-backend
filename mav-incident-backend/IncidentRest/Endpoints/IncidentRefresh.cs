536b45c9321ee23325aac7ce284a356760f7173d
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-16 18:03:31 +0100

    Implement IncidentRefresh endpoint without error handling

--- mav-incident-backend/IncidentRest/Endpoints/IncidentRefresh.cs
+++ mav-incident-backend/IncidentRest/Endpoints/IncidentRefresh.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using mav_incident_dba;
+using mav_incident_dba.Entities;
+using mav_incident_processor;
 using mav_incident_rest.HttpServer;
+using mav_incident_rest.HttpServer.Exceptions;
 using mav_incident_rest.RestService.Endpoints;
 
 namespace mav_incident_rest.IncidentRest.Endpoints
@@ -17,9 +21,22 @@ namespace mav_incident_rest.IncidentRest.Endpoints
 
         protected override RestResponse process(HttpRequest request, Dictionary<string, string> urlParams)
         {
+
+            if (!urlParams.ContainsKey("id") || !int.TryParse(urlParams["id"], out int id) || id <= 0)
+                throw new BadRequestException("ID of incident must be a positive integer.");
+
+            Incident incident = IncidentDatabase.Instance.Context.Incidents.FirstOrDefault(i => (i.ID == id));
+            if (incident == null)
+                throw new NotFoundException("Incident with the given ID is can't be found in the database.");
+
+            SingleIncidentProcessor sproc = new SingleIncidentProcessor(id);
+            sproc.Do();
+
+            // TODO: error handling
+
             RestResponse resp = RestResponse.GetDefault();
-            resp.Body = "[0, 1, 2]";
             return resp;
+
         }
 
     }
